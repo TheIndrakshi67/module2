06ff059c8fcb74d69c35d854cea3724e8897beb0
TheIndrakshi67 <CoDiNg1234!>
2026-03-22 13:44:54 +0900

    rightangletriangel

---
+++ rigthtangletringle.py
@@ -0,0 +1,6 @@
+rows=int(input("Enter number of rows " ))
+columns=int(inut("Enter umber of colomns "))
+for i in range(rows):
+    for j in range(columns):
+        print("*", end=" ")
+    print()
