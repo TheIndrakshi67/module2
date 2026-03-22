a40e482eb29b06371ec76430247b97ddea0862a3
TheIndrakshi67 <CoDiNg1234!>
2026-03-22 13:54:49 +0900

    pattern

--- rigthtangletringle.py
+++ rigthtangletringle.py
@@ -1,5 +1,5 @@
 rows=int(input("Enter number of rows " ))
-columns=int(inut("Enter umber of colomns "))
+columns=int(inut("Enter number of colomns "))
 for i in range(rows):
     for j in range(columns):
         print("*", end=" ")
