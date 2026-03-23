bb48b32be4d0ac4af643aec99a47e3646e8a284b
TheIndrakshi67 <CoDiNg1234!>
2026-03-23 20:56:40 +0900

    polygon

---
+++ polygon.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().bgcolor("purple")
+turtle.Screen().setup(300,400)
+polygon=turtle.Turtle()
+for i in range(6):
+    polygon.forward(200)
+    polygon.right(60)
+
+turtle.done()
