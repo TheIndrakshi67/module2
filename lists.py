f7895cc06154f00dc0f2e5366a3c2e5834cb10d6
TheIndrakshi67 <indrakshi.i.singh@gmail.com>
2026-04-17 18:54:40 +0900

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,8 @@
+empty_list=[]
+print("Empty list: ", empty_list)
+fruit_list=["apple","banana", "cherry"]
+print("List with elements: ", fruit_list)
+repeated_list=fruit_list*2
+print("List after* operator: ", repeated_list)
+fruit_list.reverse()
+print("Reversed list: ", fruit_list)
