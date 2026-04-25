00d0ff24fc4b930f23bdd90e4c91065babc2d5bf
TheIndrakshi67 <indrakshi.i.singh@gmail.com>
2026-04-25 22:16:10 +0900

    Add files via upload

---
+++ trigonometry.py
@@ -0,0 +1,10 @@
+import math
+angle = 45
+radians = math.radians(angle)
+sin_value = math.sin(radians)
+cos_value = math.cos(radians)
+tan_value = math.tan(radians)
+print("angle in degrees:", angle)
+print("sin:", sin_value)
+print("cos:", cos_value)
+print("tan:", tan_value)
