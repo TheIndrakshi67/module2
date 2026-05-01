f9c2e442ecc5071dcc95bbfc145d420123fef153
TheIndrakshi67 <indrakshi.i.singh@gmail.com>
2026-05-01 19:12:47 +0900

    Add files via upload

---
+++ set_intersection.py
@@ -0,0 +1,10 @@
+Basketball={"lily", "john", "David"}
+Dance={"lily", "evie", "john"}
+inntersection=Basketball.intersection(Dance)
+unioon=Basketball.union(Dance)
+ddiference=Basketball.difference(Dance)
+symmetricdifference=Basketball.symmetric_difference(Dance)
+print(inntersection)
+print(unioon)
+print(ddiference)
+print(symmetricdifference)
