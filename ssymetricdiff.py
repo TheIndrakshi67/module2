c2268c39e79a3dcda91008a7c01c78e926cc8dab
TheIndrakshi67 <indrakshi.i.singh@gmail.com>
2026-05-10 12:56:25 +0900

    Add files via upload

---
+++ ssymetricdiff.py
@@ -0,0 +1,8 @@
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+result_a = set1_a.symmetric_difference(set2_a)
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+result_b = set1_b ^ set2_b
+print("Symmetric Difference for A:", result_a) 
+print("Symmetric Difference for B:", result_b)
