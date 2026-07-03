8fcb3548922cfd308a8826fb52bd6d9251595872
TheIndrakshi67 <indrakshi.i.singh@gmail.com>
2026-07-03 16:37:48 +0900

    Add files via upload

--- project.py
+++ project.py
@@ -1,25 +1,28 @@
-class Book:
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author
-        self.is_borrowed=False
+from tkinter import *
 
-    def borrow(self):
-        self.is_borrowed=True
-        print(self.title)
-    
-    def return_book(self):
-        self.is_borrowed=False
-        print(self.title)
-book1=Book("Percy Jackson","Rick Riordan")
-book2=Book("Harry Potter","Jk Rowling")
-book3=Book("One Piece","Eiichiro Oda")
+def multiply_number():
+    number1=int(box1.get())
+    number2=int(box2.get())
+    total=number1 * number2
+    result_lbl.config(text="The product is: "+ str(total))
+window=Tk()
+window.title("Multiplication")
+window.geometry("300x200")
 
-book1.borrow()
-book1.return_book()
+lbl1=Label(window, text="Type Number 1: ")
+lbl1.pack()
+box1=Entry(window)
+box1.pack()
 
-book2.borrow()
-book2.return_book()
+lbl2=Label(window, text="Type number 2: ")
+lbl2.pack()
+box2=Entry(window)
+box2.pack()
 
-book3.borrow()
-book3.return_book()
+go_button=Button(window, text="Multiply", command=multiply_number)
+go_button.pack()
+
+result_lbl=Label(window,text="")
+result_lbl.pack()
+
+window.mainloop()
